985fbf4031ec2909f5f53fe933aecc40b65d3889
summer-bcc <summersmithd@protonmail.com>
2020-09-03 18:34:43 +0900

    Block attackers to manipulate treasury

--- contracts/Cash.sol
+++ contracts/Cash.sol
@@ -1,21 +1,194 @@
 pragma solidity ^0.5.0;
 
-import './token/ERC20.sol';
-import './owner/Ownable.sol';
-import './owner/Operator.sol';
-import './guards/ReentrancyGuard.sol';
+import "./lib/SafeMath.sol";
+import "./lib/Address.sol";
+import "./lib/SafeERC20.sol";
+import "./interfaces/IERC20.sol";
+import "./owner/Ownable.sol";
+import "./owner/Operator.sol";
+import "./guards/ReentrancyGuard.sol";
 
-contract Cash is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Operator {
+contract CashERC20 is IERC20, Context, Operator {
+    using SafeMath for uint256;
+
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(
+            recipient != operator(),
+            "basis.cash: operator as a recipient is not allowed"
+        );
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(
+            sender,
+            _msgSender(),
+            _allowances[sender][_msgSender()].sub(
+                amount,
+                "ERC20: transfer amount exceeds allowance"
+            )
+        );
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue)
+        public
+        returns (bool)
+    {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender].add(addedValue)
+        );
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        public
+        returns (bool)
+    {
+        _approve(
+            _msgSender(),
+            spender,
+            _allowances[_msgSender()][spender].sub(
+                subtractedValue,
+                "ERC20: decreased allowance below zero"
+            )
+        );
+        return true;
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(
+            amount,
+            "ERC20: transfer amount exceeds balance"
+        );
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _balances[account] = _balances[account].sub(
+            amount,
+            "ERC20: burn amount exceeds balance"
+        );
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(
+            account,
+            _msgSender(),
+            _allowances[account][_msgSender()].sub(
+                amount,
+                "ERC20: burn amount exceeds allowance"
+            )
+        );
+    }
+}
+
+contract ERC20Detailed is IERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+}
+
+contract Cash is ERC20Detailed, CashERC20 {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
     /**
-     * @notice Constructs the Basis Cash ERC-20 contract. 
+     * @notice Constructs the Basis Cash ERC-20 contract.
      */
-    constructor() public ERC20Detailed("BAC", "BAC", 18) {
-    }
-    
+    constructor() public ERC20Detailed("BAC", "BAC", 18) {}
+
     /**
      * @notice Burns basis cash from an account
      * @param from_ The address of an account to burn from
@@ -28,31 +201,35 @@ contract Cash is ERC20, ERC20Detailed, ReentrancyGuard, Ownable, Operator {
         uint256 balanceAfter = balanceOf(from_);
         return balanceBefore > balanceAfter;
     }
-    
+
     /**
-     * @notice Burns basis cash of an account from the operator 
+     * @notice Burns basis cash of an account from the operator
      * @param from_ The address of an account to burn from
      * @param amount_ The amount of basis cash to burn
      * @return whether the process has been done
      */
     function burnFrom(address from_, uint256 amount_) public returns (bool) {
-         uint256 balanceBefore = balanceOf(from_);
+        uint256 balanceBefore = balanceOf(from_);
         _burnFrom(from_, amount_);
-         uint256 balanceAfter = balanceOf(from_);
-         return balanceBefore > balanceAfter;
+        uint256 balanceAfter = balanceOf(from_);
+        return balanceBefore > balanceAfter;
     }
-    
+
     /**
      * @notice Operator mints basis cash to a recipient
      * @param recipient_ The address of recipient
-     * @param amount_ The amount of basis cash to mint to 
+     * @param amount_ The amount of basis cash to mint to
      * @return whether the process has been done
      */
-    function mint(address recipient_, uint256 amount_) public onlyOperator returns (bool) {
+    function mint(address recipient_, uint256 amount_)
+        public
+        onlyOperator
+        returns (bool)
+    {
         uint256 balanceBefore = balanceOf(recipient_);
         _mint(recipient_, amount_);
         uint256 balanceAfter = balanceOf(recipient_);
-        
+
         return balanceAfter > balanceBefore;
     }
 }
