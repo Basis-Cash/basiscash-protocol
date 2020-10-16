8183fba71bc7013d40146ac3996d35bc289a6b6c
summersmooth <winterhard1234@gmail.com>
2020-10-16 14:49:44 +0900

    Automate uniswap liquidity provision on migration

--- migrations/3_monetary_policy.js
+++ migrations/3_monetary_policy.js
@@ -1,104 +1,114 @@
+const contract = require('@truffle/contract');
 const knownContracts = require('./known-contracts');
 
-// ============ Contracts ============
+const Cash = artifacts.require('Cash');
+const Bond = artifacts.require('Bond');
+const Share = artifacts.require('Share');
+const IERC20 = artifacts.require('IERC20');
+const MockDai = artifacts.require('MockDai');
 
-// Token
-// deployed first
-const Cash = artifacts.require('Cash')
-const Bond = artifacts.require('Bond')
-const Share = artifacts.require('Share')
-const MockDai = artifacts.require('MockDai')
-
-// Rs
-// deployed second
 const Oracle = artifacts.require('Oracle')
-const IUniswapV2Factory = artifacts.require('IUniswapV2Factory')
-const MockOracle = artifacts.require('MockOracle')
 const Boardroom = artifacts.require('Boardroom')
 const Treasury = artifacts.require('Treasury')
 
-// ============ Main Migration ============
+const UniswapV2Factory = contract(require('@uniswap/v2-core/build/UniswapV2Factory.json'));
+const UniswapV2Router02 = contract(require('@uniswap/v2-periphery/build/UniswapV2Router02.json'));
+const UniswapV2Pair = contract(require('@uniswap/v2-core/build/UniswapV2Pair.json'));
 
 async function migration(deployer, network, accounts) {
+  [UniswapV2Factory, UniswapV2Router02, UniswapV2Pair].forEach(contract => {
+    contract.setProvider(web3._provider);
+    contract.defaults({ from: accounts[0] });
+  });
+
+  let uniswap, uniswapRouter;
+  if (network === 'development') {
+    console.log('Deploying uniswap on development network.');
+    await deployer.deploy(UniswapV2Factory, accounts[0]);
+    await deployer.deploy(UniswapV2Router02, accounts[0], accounts[0]);
+
+    uniswap = await UniswapV2Factory.deployed();
+    uniswapRouter = await UniswapV2Router02.deployed();
+
+  } else {
+    uniswap = await UniswapV2Factory.at(knownContracts.UniswapV2Factory[network]);
+    uniswapRouter = await UniswapV2Router02.at(knownContracts.UniswapV2Router02[network]);
+  }
+
+  const dai = network === 'mainnet'
+    ? await IERC20.at(knownContracts.DAI[network])
+    : await MockDai.deployed();
+
+  // 2. provide liquidity to BAC-DAI and BAS-DAI pair
+  // if you don't provide liquidity to BAC-DAI and BAS-DAI pair after step 1 and before step 3,
+  //  creating Oracle will fail with NO_RESERVES error.
+  const unit = web3.utils.toBN(10 ** 18).toString();
+  const max = web3.utils.toBN(10 ** 18).muln(10000).toString();
+
+  const cash = await Cash.deployed();
+  const share = await Share.deployed();
+
+  console.log('Approving Uniswap on tokens for liquidity');
+  await Promise.all([
+    approveIfNot(cash, accounts[0], uniswapRouter.address, max),
+    approveIfNot(share, accounts[0], uniswapRouter.address, max),
+    approveIfNot(dai, accounts[0], uniswapRouter.address, max),
+  ]);
+
+  // WARNING: msg.sender must hold enough DAI to add liquidity to BAC-DAI & BAS-DAI pools
+  // otherwise transaction will revert
+  console.log('Adding liquidity to pools');
+  await uniswapRouter.addLiquidity(
+    cash.address, dai.address, unit, unit, unit, unit, accounts[0], deadline(),
+  );
+  await uniswapRouter.addLiquidity(
+    share.address, dai.address, unit, unit, unit, unit, accounts[0],  deadline(),
+  );
+
+  console.log('\nPrices after providing liquidity');
+  console.log('=================================');
+  await checkPairPrice('BAC', cash.address, dai.address, uniswap, UniswapV2Pair);
+  await checkPairPrice('BAS', share.address, dai.address, uniswap, UniswapV2Pair);
+
   // Deploy boardroom
-  await deployer.deploy(Boardroom, Cash.address, Share.address)
+  await deployer.deploy(Boardroom, cash.address, share.address);
 
-  if (network === 'mainnet' || network === 'ropsten') {
-    const uniswapFactoryAddr = knownContracts.UniswapV2Factory[network]; // equal on all networks
-    const multidai = knownContracts.DAI[network];
-    const uniswap = await IUniswapV2Factory.at(uniswapFactoryAddr);
+  // 2. Deploy oracle for the pair between bac and dai
+  await deployer.deploy(
+    Oracle,
+    uniswap.address,
+    cash.address,
+    dai.address,
+  );
 
-    // 1. create pair between bac-dai, bas-dai
-    try {
-      await uniswap.createPair(Cash.address, multidai)
-      await uniswap.createPair(Share.address, multidai);
+  await deployer.deploy(
+    Treasury,
+    cash.address,
+    Bond.address,
+    share.address,
+    Oracle.address,
+    dai.address,
+    Boardroom.address,
+  );
+}
 
-    } catch (err) {
-      console.log("Warning: Failed to create pair on Uniswap. (maybe you've been already created it!)");
-      console.log(`- ${err.stack}`)
-    }
+async function checkPairPrice(tokenSymbol, tokenAddr, daiAddr, uniswap, UniswapV2Pair) {
+  const pair = await UniswapV2Pair.at(await uniswap.getPair(tokenAddr, daiAddr));
+  console.log(`${tokenSymbol} Price: ${await pair.price0CumulativeLast()} ${await pair.price1CumulativeLast()}`);
+}
 
-    // TODO: if you don't provide liquidity to BAC-DAI and BAS-DAI pair after step 1 and before step 2,
-    //  creating Oracle will fail with NO_RESERVES error.
+async function approveIfNot(token, owner, spender, amount) {
+  const allowance = await token.allowance(owner, spender);
+  if (web3.utils.toBN(allowance).gte(web3.utils.toBN(amount))) {
+    return;
+  }
+  await token.approve(spender, amount);
+  console.log(` - Approved ${token.symbol ? (await token.symbol()) : token.address}`);
+}
 
-    // 2. Deploy oracle for the pair between bac and dai
-    await deployer.deploy(
-      Oracle,
-      uniswapFactoryAddr,
-      Cash.address,
-      multidai,
-    )
-
-    await deployer.deploy(
-      Treasury,
-      Cash.address,
-      Bond.address,
-      Share.address,
-      Oracle.address,
-      multidai,
-      Boardroom.address,
-    )
-  } /*else {
-    await deployer.deploy(UniswapV2Factory, accounts[0])
-    await deployer.deploy(MockDai)
-    uniswap = new web3.eth.Contract(
-      UniswapV2Factory.abi,
-      UniswapV2Factory.address,
-    )
-    const pairAddress = await uniswap.methods
-      .createPair(Cash.address, MockDai.address)
-      .call()
-    let two = web3.utils.toBN(2).mul(web3.utils.toBN(10 ** 18))
-    let one = web3.utils.toBN(1).mul(web3.utils.toBN(10 ** 18))
-
-    // set mockup oracle of dai price fifty thousand and basis in thirty_thousand
-    await deployer.deploy(
-      MockOracle,
-      Cash.address,
-      MockDai.address,
-      two.toString(),
-      one.toString(),
-    )
-
-    await deployer.deploy(
-      Treasury,
-      Cash.address,
-      Bond.address,
-      Share.address,
-      MockOracle.address,
-      Share.address,
-      Boardroom.address,
-    )
-  }*/
-
-  let cashContract = new web3.eth.Contract(Cash.abi, Cash.address)
-  await cashContract.methods.transferOperator(Treasury.address).call()
-
-  let bondContract = new web3.eth.Contract(Bond.abi, Bond.address)
-  await bondContract.methods.transferOperator(Treasury.address).call()
-
-  let shareContract = new web3.eth.Contract(Share.abi, Share.address)
-  await shareContract.methods.transferOperator(Treasury.address).call()
+function deadline() {
+  // 30 minutes
+  return Math.floor(new Date().getTime() / 1000) + 1800;
 }
 
 module.exports = migration;
