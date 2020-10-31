a60ceb947cd9cfb455da3a45b9435f7b16a845ec
Jerry Smith <fr_sullivan@protonmail.com>
2020-10-31 22:41:37 +0900

    Use contract addresses as constructor parameter

--- migrations/8_distributor.js
+++ migrations/8_distributor.js
@@ -13,7 +13,10 @@ module.exports = async (deployer, network, accounts) => {
     ].map(distributor => distributor.deployed())
   );
 
-  await deployer.deploy(Distributor, distributors);
+  await deployer.deploy(
+    Distributor,
+    distributors.map(contract => contract.address),
+  );
   const distributor = await Distributor.deployed();
 
   console.log(`Distributor manager contract is ${distributor.address}`)
