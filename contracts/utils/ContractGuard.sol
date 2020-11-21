f822efd293d522a3024706217cf40ffe32cae220
Jerry Smith <fr_sullivan@protonmail.com>
2020-11-21 12:01:54 +0900

    Add ContractGuard for prevent flash loan attack

---
+++ contracts/utils/ContractGuard.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.12;
+
+contract ContractGuard {
+    mapping(uint256 => mapping(address => bool)) private _status;
+
+    modifier onlyOneBlock() {
+        require(!_status[block.number][msg.sender], 'ContractGuard: one block, one function');
+
+        _;
+
+        _status[block.number][msg.sender] = true;
+    }
+}
