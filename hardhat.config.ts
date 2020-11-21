8aa759da3ccda24c19b1fbdb0d163a0ed9dec394
Jerry Smith <fr_sullivan@protonmail.com>
2020-11-21 11:59:40 +0900

    Setup testing environment

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import '@nomiclabs/hardhat-ethers';
+
+export default {
+  solidity: {
+    version: '0.6.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './build/cache',
+    artifacts: './build/artifacts',
+  },
+};
