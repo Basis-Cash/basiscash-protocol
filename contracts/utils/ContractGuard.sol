6c4b2ec2aaddd6de12d786364ee1a67b44373bfd
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-02 20:40:03 +0900

    Verify msg.sender in ContractGuard

--- contracts/utils/ContractGuard.sol
+++ contracts/utils/ContractGuard.sol
@@ -3,14 +3,27 @@ pragma solidity ^0.6.12;
 contract ContractGuard {
     mapping(uint256 => mapping(address => bool)) private _status;
 
+    function checkSameOriginReentranted() internal view returns (bool) {
+        return _status[block.number][tx.origin];
+    }
+
+    function checkSameSenderReentranted() internal view returns (bool) {
+        return _status[block.number][msg.sender];
+    }
+
     modifier onlyOneBlock() {
         require(
-            !_status[block.number][tx.origin],
+            !checkSameOriginReentranted(),
+            'ContractGuard: one block, one function'
+        );
+        require(
+            !checkSameSenderReentranted(),
             'ContractGuard: one block, one function'
         );
 
         _;
 
         _status[block.number][tx.origin] = true;
+        _status[block.number][msg.sender] = true;
     }
 }
