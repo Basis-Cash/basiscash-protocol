fc8c0785d3bec80db81f96326914c5f9913f1677
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-04 22:43:57 +0900

    Update startTime of third treasury contract

--- deploy.config.ts
+++ deploy.config.ts
@@ -1,1 +1,1 @@
-export const TREASURY_START_DATE = Date.parse('2020-12-06T00:00:00Z') / 1000; // second allocation
+export const TREASURY_START_DATE = Date.parse('2020-12-09T00:00:00Z') / 1000; // third allocation
