89f88ef080be0c1d84633101fd67d7d5615506a7
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-07 00:38:49 +0900

    Add ShareWrapper virtual class & extends it

--- contracts/Boardroom.sol
+++ contracts/Boardroom.sol
@@ -9,7 +9,37 @@ import './owner/Operator.sol';
 import './utils/ContractGuard.sol';
 import './interfaces/IBasisAsset.sol';
 
-contract Boardroom is ContractGuard, Operator {
+contract ShareWrapper {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public share;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        share.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        share.safeTransfer(msg.sender, amount);
+    }
+}
+
+contract Boardroom is ShareWrapper, ContractGuard, Operator {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
@@ -18,7 +48,6 @@ contract Boardroom is ContractGuard, Operator {
     /* ========== DATA STRUCTURES ========== */
 
     struct Boardseat {
-        uint256 shares;
         uint256 lastSnapshotIndex;
         uint256 rewardPerSharePaid;
         uint256 rewardEarned;
@@ -33,7 +62,6 @@ contract Boardroom is ContractGuard, Operator {
     /* ========== STATE VARIABLES ========== */
 
     IERC20 private cash;
-    IERC20 private share;
 
     uint256 private totalShares;
 
@@ -55,7 +83,7 @@ contract Boardroom is ContractGuard, Operator {
     /* ========== Modifiers =============== */
     modifier directorExists {
         require(
-            directors[msg.sender].shares > 0,
+            balanceOf(msg.sender) > 0,
             'Boardroom: The director does not exist'
         );
         _;
@@ -99,10 +127,6 @@ contract Boardroom is ContractGuard, Operator {
 
     // =========== Director getters
 
-    function shareOf(address director) public view returns (uint256) {
-        return directors[director].shares;
-    }
-
     function rewardPerShare() public view returns (uint256) {
         return getLatestSnapshot().rewardPerShare;
     }
@@ -112,7 +136,7 @@ contract Boardroom is ContractGuard, Operator {
         uint256 storedRPS = getLastSnapshotOf(director).rewardPerShare;
 
         return
-            shareOf(director).mul(latestRPS.sub(storedRPS)).div(1e18).add(
+            balanceOf(director).mul(latestRPS.sub(storedRPS)).div(1e18).add(
                 directors[director].rewardEarned
             );
     }
@@ -121,46 +145,29 @@ contract Boardroom is ContractGuard, Operator {
 
     function stake(uint256 amount)
         public
+        override
         onlyOneBlock
         updateReward(msg.sender)
     {
         require(amount > 0, 'Boardroom: Cannot stake 0');
-
-        // Update director's boardseat
-        Boardseat memory director = directors[msg.sender];
-        director.shares = director.shares.add(amount);
-        directors[msg.sender] = director;
-
-        // Update totalShares & transfer cash
-        totalShares = totalShares.add(amount);
-        share.safeTransferFrom(msg.sender, address(this), amount);
+        super.stake(amount);
         emit Staked(msg.sender, amount);
     }
 
     function withdraw(uint256 amount)
         public
+        override
         onlyOneBlock
         directorExists
         updateReward(msg.sender)
     {
         require(amount > 0, 'Boardroom: Cannot withdraw 0');
-
-        // Update director's boardseat
-        uint256 directorShare = shareOf(msg.sender);
-        require(
-            directorShare >= amount,
-            'Boardroom: withdraw request greater than staked amount'
-        );
-        directors[msg.sender].shares = directorShare.sub(amount);
-
-        // Update totalShares & transfer cash
-        totalShares = totalShares.sub(amount);
-        share.safeTransfer(msg.sender, amount);
+        super.withdraw(amount);
         emit Withdrawn(msg.sender, amount);
     }
 
     function exit() external {
-        withdraw(shareOf(msg.sender));
+        withdraw(balanceOf(msg.sender));
         claimReward();
     }
 
