c9d6e7ab55b0c8922923c95a7c528eee2309516f
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-06 16:46:48 +0900

    Update oracle period to 24 hours

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -14,7 +14,7 @@ import './interfaces/IUniswapV2Factory.sol';
 contract Oracle {
     using FixedPoint for *;
 
-    uint256 public constant PERIOD = 10 minutes;
+    uint256 public constant PERIOD = 24 hours;
 
     IUniswapV2Pair public pair;
     address public token0;
