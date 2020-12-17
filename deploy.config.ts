2fdd920fac7c2a1e7bfa967376814ecf0dad3c4e
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-18 06:18:13 +0900

    Change deploy config

--- deploy.config.ts
+++ deploy.config.ts
@@ -1,4 +1,4 @@
-export const TREASURY_START_DATE = Date.parse('2020-12-12T00:00:00Z') / 1000;
-export const ORACLE_START_DATE = Date.parse('2020-12-09T00:00:00Z') / 1000;
+export const TREASURY_START_DATE = Date.parse('2020-12-20T00:00:00Z') / 1000;
+export const ORACLE_START_DATE = Date.parse('2020-12-19T00:00:00Z') / 1000;
 export const UNI_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 export const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
