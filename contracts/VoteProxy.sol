0a0d2b21b1ff7df2f43e802800419bb225fce423
Jerry Smith <fr_sullivan@protonmail.com>
2020-12-18 04:13:52 +0900

    Add VoteProxy contract for snapshot.page

---
+++ contracts/VoteProxy.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+import './owner/Operator.sol';
+
+contract VoteProxy is Operator {
+    // Events
+    event BoardroomChanged(
+        address indexed operator,
+        address indexed oldBoardroom,
+        address indexed newBoardroom
+    );
+
+    // Boardroom
+    address public boardroom;
+
+    constructor(address _boardroom) public {
+        boardroom = _boardroom;
+    }
+
+    function setBoardroom(address newBoardroom) public onlyOperator {
+        address oldBoardroom = boardroom;
+        boardroom = newBoardroom;
+        emit BoardroomChanged(msg.sender, oldBoardroom, newBoardroom);
+    }
+
+    function decimals() external pure returns (uint8) {
+        return uint8(18);
+    }
+
+    function name() external pure returns (string memory) {
+        return 'BAS in Boardroom';
+    }
+
+    function symbol() external pure returns (string memory) {
+        return 'SBAS';
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return IERC20(boardroom).totalSupply();
+    }
+
+    function balanceOf(address _voter) external view returns (uint256) {
+        return IERC20(boardroom).balanceOf(_voter);
+    }
+}
