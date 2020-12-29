511291bc691b83975c966830f8d0deabfef0c647
stevenkcolin <steven.k.colin@gmail.com>
2020-12-29 23:11:39 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -26,7 +26,7 @@
 
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const mnemonic = "vicious patient purse paper black runway afraid length run air gaze fancy";
+const mnemonic = "secret";
 const INFURA_API_KEY="65ea3ed956c44aca8c46cb48a782712b";
 
 
