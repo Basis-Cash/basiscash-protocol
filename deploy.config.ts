e2deb1e82be53b1dbba1971ab72ae14e651a7f7f
Jerry Smith <fr_sullivan@protonmail.com>
2021-01-11 06:51:56 +0900

    Add new deployment

--- deploy.config.ts
+++ deploy.config.ts
@@ -1,4 +1,8 @@
-export const TREASURY_START_DATE = Date.parse('2020-12-22T00:00:00Z') / 1000;
-export const ORACLE_START_DATE = Date.parse('2020-12-19T00:00:00Z') / 1000;
+export const TREASURY_START_DATE = Date.parse('2021-01-13T00:00:00Z') / 1000;
+export const ORACLE_START_DATE = Date.parse('2021-01-11T00:00:00Z') / 1000;
+export const CURVE_MIN_SUPPLY = '0';
+export const CURVE_MAX_SUPPLY = '250000000';
+export const CURVE_MIN_CEILING = '1.01';
+export const CURVE_MAX_CEILING = '1.05';
 export const UNI_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 export const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
