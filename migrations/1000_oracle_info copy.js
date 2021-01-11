4d0ca7f69dddd8c9c525f28603387582b8858c99
stevenkcolin <steven.k.colin@gmail.com>
2021-01-11 09:11:21 +0800

    submit

---
+++ migrations/1000_oracle_info copy.js
@@ -0,0 +1,41 @@
+const Oracle = artifacts.require('Oracle');
+
+module.exports = async (deployer, network, accounts) => {
+  const oracle = await Oracle.deployed();
+  await oracle.update();
+  console.log("oracle updated");
+
+
+
+  const token0 = await oracle.token0.call();
+  const token1 = await oracle.token1.call();
+  console.log("token0: ",token0.toString());
+  console.log("token1: ",token1.toString());
+
+  const pair = await oracle.pair.call();
+  console.log("pair: ",pair.toString()); //oracle is bac-dai oracle
+
+  const blockTimestampLast = web3.utils.BN(await oracle.blockTimestampLast.call()).toString();
+  console.log("blockTimestampLast: ",blockTimestampLast); 
+
+
+  const price0CumulativeLast = web3.utils.BN(await oracle.price0CumulativeLast.call()).toString();
+  console.log("price0CumulativeLast: ",price0CumulativeLast); 
+
+  const price1CumulativeLast = web3.utils.BN(await oracle.price1CumulativeLast.call()).toString();
+  console.log("price1CumulativeLast: ",price1CumulativeLast); 
+
+
+  const price0Average = web3.utils.BN(await oracle.price0Average.call()).toString();
+  console.log("price0Average: ",price0Average); 
+
+  const price1Average = web3.utils.BN(await oracle.price1Average.call()).toString();
+  console.log("price1Average: ",price1Average); 
+
+  const amountIn = web3.utils.toBN(10 ** 18);
+  const price = web3.utils.BN(await oracle.consult(token0,amountIn)).toString();
+  console.log("oracle.consult result is: ",price);
+
+
+
+}
---
+++ migrations/1001_boardroom_info.js
@@ -0,0 +1,66 @@
+const Boardroom = artifacts.require('Boardroom');
+const Treasury = artifacts.require('Treasury');
+const Cash = artifacts.require('Cash');
+const Bond = artifacts.require('Bond');
+const Share = artifacts.require('Share');
+const Timelock = artifacts.require('Timelock');
+const Oracle = artifacts.require('Oracle');
+
+
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const DAY = 86400;
+const SEIGNIORAGE_AMOUNT = web3.utils.toBN(10 ** 18).muln(10000).toString();
+const oneETH = web3.utils.toBN(10 ** 18).muln(1).toString();
+
+
+module.exports = async (deployer, network, accounts) => {
+  const boardroom = await Boardroom.deployed();
+  console.log("boardroom address is: ",boardroom.address);
+
+  // -------------- begin bac minted 10000 and allocateSeigniorage -----------------------
+  // const cash = await Cash.deployed();
+  // console.log("cash address is: ",cash.address);
+
+  // const treasury = await Treasury.deployed();
+  // console.log("treasury address is: ",treasury.address);
+
+  // const timelock = await Timelock.deployed();
+  // console.log("timelock address is: ",timelock.address);
+
+  // console.log("cash minted ",SEIGNIORAGE_AMOUNT);
+  // await cash.mint(treasury.address, SEIGNIORAGE_AMOUNT);
+  // await cash.approve(boardroom.address, SEIGNIORAGE_AMOUNT);
+
+  // console.log("booardroom allocateSeigniorage");
+  // await boardroom.allocateSeigniorage(SEIGNIORAGE_AMOUNT)
+  // -------------- end bac minted 10000 and allocateSeigniorage -----------------------
+
+
+  const latestSnapshotIndex = await boardroom.latestSnapshotIndex()
+  console.log("latestSnapshotIndex: ",latestSnapshotIndex.toString());
+
+  const address0 = accounts[0];
+  const getLastSnapshotIndexOf = await boardroom.getLastSnapshotIndexOf(address0);
+  console.log("getLastSnapshotIndexOf: ",getLastSnapshotIndexOf.toString());
+
+
+  const rewardPerShare = await boardroom.rewardPerShare()
+  console.log("rewardPerShare: ",rewardPerShare.toString());
+
+  const earned = await boardroom.earned(address0);
+  console.log("earned: ",earned.toString());
+
+  // it send tx
+  // await boardroom.stake(oneETH);
+  // console.log("boardroom.staked: ",oneETH);
+
+  // it send tx
+  // await boardroom.withdraw(oneETH);
+  // console.log("boardroom.withdraw: ",oneETH);
+
+  // it send tx
+  await boardroom.claimReward();
+  console.log("boardroom.claimReward");
+
+}
