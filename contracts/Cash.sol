a430ece5a5a774aaaa733e30a61e89b5d3b438f0
eepdev <78463658+eepdev@users.noreply.github.com>
2021-02-05 00:14:30 -0500

    revert

--- contracts/Cash.sol
+++ contracts/Cash.sol
@@ -2,14 +2,8 @@ pragma solidity ^0.6.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
 import './owner/Operator.sol';
-import './interfaces/IFeeChecker.sol';
-import './interfaces/IFeeDistributor.sol';
 
 contract Cash is ERC20Burnable, Operator {
-    
-    address public feeChecker; //Checks whether a transaction is taxed and returns the remaining amount and tax
-    address public feeDistributor; //Handles distributing the tax
-
     /**
      * @notice Constructs the Basis Cash ERC-20 contract.
      */
@@ -55,49 +49,5 @@ contract Cash is ERC20Burnable, Operator {
         onlyOperator
     {
         super.burnFrom(account, amount);
-    }
-    
-    function _transfer(address sender, address recipient, uint256 amount) 
-        internal 
-        override 
-    {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        if (amount > 0) {
-            _beforeTokenTransfer(sender, recipient, amount);
-            _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-
-            if(feeChecker.isTransferTaxed(sender, recipient)) {
-                uint256 feeAmount = IFeeChecker(feeChecker).calculateFeeAmount(sender, recipient, amount);
-                uint256 transferToAmount = amount.sub(feeAmount);
-
-                require(transferToAmount.add(feeAmount) == amount, "transferToAmount + feeAmount != amount");
-
-                _balances[recipient] = _balances[recipient].add(transferToAmount);
-                emit Transfer(sender, recipient, transferToAmount);
-
-                if(feeAmount > 0) {
-                    _balances[feeDistributor] = _balances[feeDistributor].add(feeAmount);
-                    emit Transfer(sender, feeDistributor, feeAmount);
-                    IFeeDistributor(feeDistributor).addFee(feeAmount);
-                }
-            }
-            else {
-                _balances[recipient] = _balances[recipient].add(amount);
-                emit Transfer(sender, recipient, amount);
-            }
-        }
-        else {
-            emit Transfer(sender, recipient, amount);
-        }
-    }
-
-    function setFeeCheckerAddress(address _address) public onlyOperator {
-        feeChecker = _address;
-    }
-
-    function setFeeDistributorAddress(address _address) public onlyOperator {
-        feeDistributor = _address;
     }
 }
