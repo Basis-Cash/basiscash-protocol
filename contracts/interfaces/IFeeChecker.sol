a3bec3c156ae46425f95b6e1e1b8414b9b1152eb
eepdev <78463658+eepdev@users.noreply.github.com>
2021-02-22 13:37:55 -0500

    Update IFeeChecker.sol
    Fixed function definition

--- contracts/interfaces/IFeeChecker.sol
+++ contracts/interfaces/IFeeChecker.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.0;
 
 interface IFeeChecker {
 
-    function isTransferTaxed(address sender, address recipient) external returns (bool);
+    function isTransferTaxed(address sender, address recipient) external view returns (bool);
 
-    function calculateFeeAmount(address sender, address recipient, uint256 amount) external returns (uint256 feeAmount);
+    function calculateFeeAmount(address sender, address recipient, uint256 amount) external view returns (uint256 feeAmount);
 }
